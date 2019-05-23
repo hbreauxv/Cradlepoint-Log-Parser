4015ed90b8a51a0843402a8b9c45c63fd94f44d2
Harvey Breaux <hbreauxv@gmail.com>
2019-05-23 13:52:34 -0600

    initial version of restructure

---
+++ log_analyzer/analyze_log.py
@@ -0,0 +1,29 @@
+"""
+Preforms various log analysis functions. Current vision is that class will handle requests to scan logs, make graphs,
+etc...
+"""
+
+import sys
+
+class AnalyzeLog(object):
+
+    def __init__(self, log, search_categories):
+        self.log = log
+        self.search_categories = search_categories
+
+    def search_log(self):
+        """Uses compiled regex to search log and export meaning file"""
+        pass
+
+    def _update_categories(self):
+        """Update search_categories"""
+
+
+if __name__ == "__main__":
+    try:
+        AnalyzeLog(sys.argv[1], search_categories='all').search_log()
+        print('Searched logs for errors successfully')
+
+    except Exception as e:
+        print('Exception: %s' % e)
+        print('\nUsage: search_log_01.py <input_file> <output_file>')
