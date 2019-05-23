01f707b753d13ce6801c1a4150077801be3474d9
Harvey Breaux <hbreauxv@gmail.com>
2019-05-23 13:49:11 -0600

    renamed unit test

--- Tests/Unit/scan_log_tests.py
+++ Tests/Unit/scan_log_tests.py
@@ -4,13 +4,15 @@ Basic test cases for search_log_01.py
 
 import unittest
 import filecmp
-from search_log_01 import search_log
+import os
+import sys
+from scan_log import ScanLog
 
 class TestSearch(unittest.TestCase):
 
     def test_short_search(self):
         # Compare the output from searching a short log file
-        search_log('short_test.txt','compare_short_test.txt')
+        ScanLog('short_test.txt','compare_short_test.txt', '').search_log()
         self.assertTrue(filecmp.cmp('correct_short_test.txt','compare_short_test.txt'),
                                     'Short search failed, output of search log didnt match correct_short_test.txt')
 
